28e87d5c7f9133b0ddd993e7a57b6120e0306399
Titusz Pan <titusz.pan@gmail.com>
2021-12-26 11:57:18 +0100

    Add a couple of tests.

--- tests/test_jcs.py
+++ tests/test_jcs.py
@@ -16,6 +16,11 @@ def test_canonicalize_int_key():
         assert jcs.canonicalize({4: "tag"})
 
 
+def test_canonicalize_nums():
+    nums = dict(int=654635, float=26746.68731)
+    assert jcs.canonicalize(nums) == b'{"float":26746.68731,"int":654635}'
+
+
 def test_canonicalize_big_num():
 
     n = 2 ** 53 - 1
@@ -29,3 +34,12 @@ def test_canonicalize_big_num():
     assert c == b'{"tag":9007199254740992}'
     with pytest.raises(AssertionError):
         assert json.loads(c)["tag"] == n
+
+
+def test_canonicalize_tuple():
+    assert jcs.canonicalize(("A", "tuple", 7)) == b'["A","tuple",7]'
+
+
+def test_canonicalize_set():
+    with pytest.raises(TypeError):
+        assert jcs.canonicalize({1, 2, 4}) == b""
