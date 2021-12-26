df6cc1bcfdd5f0efa71aa7b6ba86758c12001b72
Titusz Pan <titusz.pan@gmail.com>
2021-12-26 11:51:40 +0100

    Add some small tests

--- tests/test_jcs.py
+++ tests/test_jcs.py
@@ -1,4 +1,6 @@
+import pytest
 import jcs
+import json
 
 
 def test_version():
@@ -7,3 +9,23 @@ def test_version():
 
 def test_canonicalize():
     assert jcs.canonicalize({"tag": 4}) == b'{"tag":4}'
+
+
+def test_canonicalize_int_key():
+    with pytest.raises(AttributeError):
+        assert jcs.canonicalize({4: "tag"})
+
+
+def test_canonicalize_big_num():
+
+    n = 2 ** 53 - 1
+    c = jcs.canonicalize({"tag": n})
+    assert c == b'{"tag":9007199254740991}'
+    assert json.loads(c)["tag"] == n
+
+    # overflow
+    n = 2 ** 53 + 1
+    c = jcs.canonicalize({"tag": n})
+    assert c == b'{"tag":9007199254740992}'
+    with pytest.raises(AssertionError):
+        assert json.loads(c)["tag"] == n
